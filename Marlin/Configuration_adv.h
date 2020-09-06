b333de025b4dbdf193d1d006f160b4007ec362a8
Minn Soe <contributions@minn.io>
2020-09-06 14:35:11 +0100

    Override stepper timing for iHSV42 step dir servo control

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1809,8 +1809,8 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-//#define MINIMUM_STEPPER_POST_DIR_DELAY 650
-//#define MINIMUM_STEPPER_PRE_DIR_DELAY 650
+#define MINIMUM_STEPPER_POST_DIR_DELAY 6000
+#define MINIMUM_STEPPER_PRE_DIR_DELAY 6000
 
 /**
  * Minimum stepper driver pulse width (in µs)
@@ -1823,7 +1823,7 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-//#define MINIMUM_STEPPER_PULSE 2
+#define MINIMUM_STEPPER_PULSE 3
 
 /**
  * Maximum stepping rate (in Hz) the stepper driver allows
@@ -1837,7 +1837,7 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-//#define MAXIMUM_STEPPER_RATE 250000
+#define MAXIMUM_STEPPER_RATE 200000
 
 // @section temperature
 
